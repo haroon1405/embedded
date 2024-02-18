0032388f525171ff69c5c1b9fc2a78168fd93fc9
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-02-18 22:25:51 +0400

    update assign1.c

--- embedded_c_basics/assign1.c
+++ embedded_c_basics/assign1.c
@@ -2,19 +2,17 @@
 
 #include <stdio.h>
 
-int findmax(int a, int b);
-
 int main(){
     int num1=0, num2=0, max_num=0;
     printf("Enter the first number: ");
     scanf("%d", &num1);
     printf("Enter the second number: ");
     scanf("%d", &num2);
-    max_num = findmax(num1,num2);
+    if(num1==num2){
+        printf("Numbers are equal");
+        return 0;
+    }
+    max_num = (num1 > num2)? num1 : num2;
     printf("The maximum number is : %d",max_num);
     return 0;
-}
-
-int findmax(int a, int b){
-    return (a > b)? a : b;
 }
