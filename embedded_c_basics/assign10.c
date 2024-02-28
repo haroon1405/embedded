714b5ee23529d03519a9c43142d00cc97c5e8249
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-02-28 20:58:13 +0400

    assign10.c

---
+++ embedded_c_basics/assign10.c
@@ -0,0 +1,19 @@
+// Write a Function to find sum of 2 numbers.
+
+#include <stdio.h>
+
+int sum(int a, int b);
+
+int main(){
+    int num1=0,num2=0,result=0;
+    printf("Enter the first number : ");
+    scanf("%d",&num1);
+    printf("Enter the second number : ");
+    scanf("%d",&num2);
+    result = sum(num1,num2);
+    printf("The sum is %d",result);
+}
+
+int sum(int a, int b){
+    return a+b;
+}
